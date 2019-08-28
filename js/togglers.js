3412d717a39d8910e67f859a956c0b49f38a662d
Dunkelheyt <dalmadxx@gmail.com>
2019-08-27 22:57:01 -0700

    Huge fixes, added more information about food

--- js/togglers.js
+++ js/togglers.js
@@ -2,4 +2,68 @@ $(document).ready(function(){
   $("h1.toggler").click(function(){
     $("li.initiallyHidden").toggle(250);
   });
+
+  $("p.toggler").click(function(){
+    $("div.initiallyHidden").toggle(250);
+  });
+
+  $("p.toggler2").click(function(){
+    $("div.initiallyHidden2").toggle(250);
+  });
+
+  $("p.toggler3").click(function(){
+    $("div.initiallyHidden3").toggle(250);
+  });
+
+  $("p.toggler4").click(function(){
+    $("div.initiallyHidden4").toggle(250);
+  });
+
+  $("p.toggler5").click(function(){
+    $("div.initiallyHidden5").toggle(250);
+  });
+
+  $("p.toggler6").click(function(){
+    $("div.initiallyHidden6").toggle(250);
+  });
+
+  $("p.toggler7").click(function(){
+    $("div.initiallyHidden7").toggle(250);
+  });
+
+  $("p.toggler8").click(function(){
+    $("div.initiallyHidden8").toggle(250);
+  });
+
+  $("p.toggler9").click(function(){
+    $("div.initiallyHidden9").toggle(250);
+  });
+
+  $("p.toggler10").click(function(){
+    $("div.initiallyHidden10").toggle(250);
+  });
+
+  $("p.toggler11").click(function(){
+    $("div.initiallyHidden11").toggle(250);
+  });
+
+  $("p.toggler12").click(function(){
+    $("div.initiallyHidden12").toggle(250);
+  });
+
+  $("p.toggler13").click(function(){
+    $("div.initiallyHidden13").toggle(250);
+  });
+
+  $("p.toggler14").click(function(){
+    $("div.initiallyHidden14").toggle(250);
+  });
+
+  $("p.toggler15").click(function(){
+    $("div.initiallyHidden15").toggle(250);
+  });
+
+  $("p.toggler16").click(function(){
+    $("div.initiallyHidden16").toggle(250);
+  });
 });
